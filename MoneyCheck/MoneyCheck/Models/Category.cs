72e2095337de0e4367de0447a1b9dea04bf4cb13
Hokku <darksouls2make@gmail.com>
2024-07-29 23:11:20 +0400

    Revert "Add Models"
    This reverts commit cc3298e928d0cfd95f04a807fe856e6545136fb6.

--- MoneyCheck/MoneyCheck/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MoneyCheck.Models;
-
-public class Category
-{
-    public int CategoryId { get; set; }
-    public List<string> Categories { get; set; } = new List<string>();
-    public int AccountId { get; set; }
-    public Account AccountNavigation { get; set; }
-}
--- Troelsen/AutoLot.Samples/Models/Make.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoLot.Samples.Models;
-
-public class Make : BaseEntity
-{
-    
-    public string Name { get; set; }
-    public IEnumerable<Car> Cars { get; set; } = new List<Car>();
-
-}
