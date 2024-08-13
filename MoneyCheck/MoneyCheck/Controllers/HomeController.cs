63917fbf448d084e382a85152a37277488b6ccc5
Hokku <darksouls2make@gmail.com>
2024-08-13 23:08:53 +0400

    Wip

--- MoneyCheck/MoneyCheck/Controllers/HomeController.cs
+++ MoneyCheck/MoneyCheck/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using MoneyCheck.DataAcess.EntityFramework;
 using MoneyCheck.Models;
@@ -9,12 +10,20 @@ namespace MoneyCheck.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly DataContext _context;
+    private readonly DbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DbContext context)
     {
+        _context = context;
         _logger = logger;
     }
+
+   /* public IActionResult CreateUser(User user)
+    {
+        _context.Add(new User() { Id = 1, UserName = "Hokku", Balance = 0, UserAccounts = new List<Account>() });
+        _context.SaveChanges();
+        return View();
+    } */
     
     public IActionResult Index()
     {
